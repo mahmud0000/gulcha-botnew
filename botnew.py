def42ab6dbd1a0e8a562e3d73418d5bfa26daee2
mahmud0000 <135305143+mahmud0000@users.noreply.github.com>
2026-05-06 20:16:25 +0500

    Update botnew.py

--- botnew.py
+++ botnew.py
@@ -295,8 +295,7 @@ const doc = new Document({
             ]
           }),
           ...[
-                  ["BOT_TOKEN", "8766748683:AAFJ6cKF56fTORAISb6uQzuojpHRTxRJVGI
-)"],
+TOKEN = os.getenv("8766748683:AAFJ6cKF56fTORAISb6uQzuojpHRTxRJVGI")
 )"],
             ["ADMIN_ID", "5594795335
 ) — @userinfobot dan oling"],
